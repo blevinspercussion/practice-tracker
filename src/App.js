24b89871f8c294d84af65d924b0bbccfdd566ff7
blevinspercussion <blevinspercussion@gmail.com>
2022-07-12 14:07:29 -0500

    Commit so I can merge

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import './fonts/Pacifico-Regular.ttf'
 
 function App() {
   return (
     <div className="App">
-
+      <h1 className='title'>Practice Tracker</h1>
     </div>
   );
 }
