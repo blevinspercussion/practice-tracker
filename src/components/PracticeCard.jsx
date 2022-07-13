a175d6e6a742a36b67a19d63a6bee796e5bd6d00
Adam Blevins <blevinspercussion@gmail.com>
2022-07-13 13:54:03 -0500

    PracticeCards now change background to green when clicked, signifying that the item has been completed

--- src/components/PracticeCard.jsx
+++ src/components/PracticeCard.jsx
@@ -1,8 +1,16 @@
 import './components.css';
 
+
+
 function PracticeCard ({ title, description, time }) {
+
+    const cardCompleteHandler = () => {
+        const card = document.getElementById(title);
+        card.classList.toggle('card-complete');
+    }
+
     return (
-        <div className='card'>
+        <div id={title} className='card' onClick={cardCompleteHandler}>
             <h3>{title}</h3>
             <p>{description}</p>
             <p>Time: {time} minutes</p>
