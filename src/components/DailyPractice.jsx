81ddc68d35080c404f185f6d585bd22509857e20
Adam Blevins <blevinspercussion@gmail.com>
2022-07-13 12:52:34 -0500

    Added placeholder text on the DailyPractice and Goals components that will display if user hasn't set any practice items or goals

--- src/components/DailyPractice.jsx
+++ src/components/DailyPractice.jsx
@@ -2,37 +2,47 @@ import './components.css';
 import PracticeCard from './PracticeCard';
 
 function DailyPractice ({ headings, dailyPractice }) {
-    return (
-        <div className="page-layout">
-            {headings.map((heading, index) => {
-                return (
-                    <div key={index}>
-                        <li><h1>{heading}</h1></li>
-                        <hr className='heading-divider' />
-                        <div className='practice-section'>
-                        <ul className='card-list'>
-                            {dailyPractice.map((practice, index) => {
-                                if (practice.heading === heading) {
-                                    return (
-                                        <li>
-                                        <PracticeCard 
-                                            index={index}
-                                            heading={practice.heading}
-                                            title={practice.title}
-                                            description={practice.description}
-                                            time={practice.time}
-                                            />
-                                        </li>
-                                    )
-                                }
-                            })}
-                        </ul>
+
+    if (dailyPractice.length === 0 || headings.length === 0) {
+        return (
+            <div className='page-layout'>
+                <p>Your daily practice routine is currently empty. Try adding some practice items on the Admin Panel, then check back here!</p>
+            </div>
+        )
+    } else {
+
+        return (
+            <div className="page-layout">
+                {headings.map((heading, index) => {
+                    return (
+                        <div key={index}>
+                            <li><h1>{heading}</h1></li>
+                            <hr className='heading-divider' />
+                            <div className='practice-section'>
+                            <ul className='card-list'>
+                                {dailyPractice.map((practice, index) => {
+                                    if (practice.heading === heading) {
+                                        return (
+                                            <li>
+                                            <PracticeCard 
+                                                index={index}
+                                                heading={practice.heading}
+                                                title={practice.title}
+                                                description={practice.description}
+                                                time={practice.time}
+                                                />
+                                            </li>
+                                        )
+                                    }
+                                })}
+                            </ul>
+                            </div>
                         </div>
-                    </div>
-                )
-            })}
-        </div>
+                    )
+                })}
+            </div>
     )
+            }
 }
 
 export default DailyPractice;
--- src/components/Goals.jsx
+++ src/components/Goals.jsx
@@ -2,41 +2,52 @@ import './components.css';
 import GoalCard from './GoalCard';
 
 function Goals ({ goals }) {
-    return (
-        <div className='page-layout'>
-            <h1>Short Term Goals</h1>
-                <ul className='card-list'>
-                {goals.map((goal, index) => {
-                    if (goal.type === 'short') {
-                        return(
-                            <GoalCard 
-                                key={index}
-                                type={goal.type}
-                                title={goal.title}
-                                description={goal.description}
-                                />
-                        )
-                    }
-                    
-                })}
-                </ul>
-            <h1>Long Term Goals</h1>
-                <ul className='card-list'>
-                {goals.map((goal, index) => {
-                    if (goal.type === 'long') {
-                        return (
-                            <GoalCard
-                                key={index}
-                                type={goal.type}
-                                title={goal.title}
-                                description={goal.description}
-                                />
-                        )
-                    }
-                })}
-                </ul>
-        </div>
-    )
+
+    if (goals.length === 0) {
+        return (
+            <div className='page-layout'>
+                <p>You don't have any goals set at the moment. Try adding some goals on the Admin Panel, then check back here!.</p>
+            </div>
+        )
+    } else {
+
+
+        return (
+            <div className='page-layout'>
+                <h1>Short Term Goals</h1>
+                    <ul className='card-list'>
+                    {goals.map((goal, index) => {
+                        if (goal.type === 'short') {
+                            return(
+                                <GoalCard 
+                                    key={index}
+                                    type={goal.type}
+                                    title={goal.title}
+                                    description={goal.description}
+                                    />
+                            )
+                        }
+                        
+                    })}
+                    </ul>
+                <h1>Long Term Goals</h1>
+                    <ul className='card-list'>
+                    {goals.map((goal, index) => {
+                        if (goal.type === 'long') {
+                            return (
+                                <GoalCard
+                                    key={index}
+                                    type={goal.type}
+                                    title={goal.title}
+                                    description={goal.description}
+                                    />
+                            )
+                        }
+                    })}
+                    </ul>
+            </div>
+        )
+                }
 }
 
 export default Goals;
